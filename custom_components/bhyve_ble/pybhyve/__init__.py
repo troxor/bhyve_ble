d84fb0bf87816276e112e46917311b11cfd8c6d4
Troy C. <327529+troxor@users.noreply.github.com>
2026-08-07 14:42:39 -0500

    ci: implement Release Please

--- custom_components/bhyve_ble/pybhyve/__init__.py
+++ custom_components/bhyve_ble/pybhyve/__init__.py
@@ -3,7 +3,7 @@
 from .constants import MAX_TIMER_PORTS, MAX_TIMER_STATION_ID, VALID_TIMER_PORT_COUNTS
 from .gen2_codec import merge_gen2_decoded
 
-__version__ = "0.1.0"
+__version__ = "0.1.0"  # x-release-please-version
 
 __all__ = [
     "MAX_TIMER_PORTS",
